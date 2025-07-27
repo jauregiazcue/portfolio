2ab1a6bb850e75461c7dc6065944256bcc2d6c24
Kai <jauregiazcue@gmail.com>
2025-07-27 15:48:49 +0200

    added footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -5,8 +5,22 @@ function Footer() {
   return (
     <footer id="contact" className="footer">
       <section className='footer__section--merger'></section>
-      <section className='footer__section--info'>© 2025 Mi App</section>
-      
+      <section className='footer__section--info'>
+        <ul className='footer__ul'>
+          <li>
+            <a target="_blank" href="https://www.linkedin.com/in/jauregiazcue/">
+              <i class="fa-brands fa-linkedin"></i>
+            </a>
+          </li>
+          <li>
+            <a target="_blank" href="https://github.com/jauregiazcue">
+              <i class="fa-brands fa-square-github"></i>
+            </a>
+          </li>
+        </ul>
+        <p>© Kai Jauregi Azcue. All rights reserved.</p>
+      </section>
+
     </footer>
   );
 }
