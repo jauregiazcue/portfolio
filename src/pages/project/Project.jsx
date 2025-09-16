03577857a3ead8e58a31d27b185494cf48e8de0c
Kai <jauregiazcue@gmail.com>
2025-09-16 19:51:24 +0200

    improve responsiveness with project page

--- src/pages/project/Project.jsx
+++ src/pages/project/Project.jsx
@@ -2,35 +2,35 @@ import './Project.css'
 import ProjectCard from "../../components/projects/ProjectCard"
 import ProjectList from "../../components/projects/ProjectList"
 import projects from "../../assets/projectData.json"
-import { useState } from 'react';
+import { useState } from 'react'
 import { useMediaQuery } from 'react-responsive'
+
+
+
+function MobileProject() {
+    return <>
+        {projects.map((element) => {
+            return <>
+                <section className='project'>
+                    <ProjectCard project={element} />
+                </section>
+                <hr />
+            </>
+        })
+        }
+    </>
+}
 
 function Project() {
     const [activeId, setActiveId] = useState(0);
     const isTabletOrMobile = useMediaQuery({ query: '(max-width: 768px)' })
 
     return !isTabletOrMobile ?
-        (
-
-            <section className='project'>
-                <ProjectCard project={projects[activeId]} />
-                <ProjectList setActiveObjectId={setActiveId} projects={projects} />
-            </section>
-
-        ) :
-        (
-            <>
-                {projects.map((element) => {
-                    return <>
-                        <section className='project'>
-                            <ProjectCard project={element} />
-
-                        </section>
-                        <hr />
-                    </>
-                })}
-            </>
-        )
+        <section className='project'>
+        <ProjectCard project={projects[activeId]} />
+        <ProjectList setActiveObjectId={setActiveId} projects={projects} />
+    </section>
+        : <MobileProject />
 }
 
 export default Project; 
