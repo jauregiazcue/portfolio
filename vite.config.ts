5c0aac7e365510ea55e19129c7be4a29748535ff
Kai Jauregi <jauregiazcue@gmail.com>
2026-09-05 11:34:06 +0200

    changed package to fix browserRouter problems with github

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { fileURLToPath } from "url";
 export default defineConfig({
 
   plugins: [react()],
+  base: "./",
   resolve: {
     alias: {
       "@": fileURLToPath(new URL('./src', import.meta.url)),
@@ -15,6 +16,4 @@ export default defineConfig({
       "@interfaces":  fileURLToPath(new URL('./src/utils/interfaces', import.meta.url)),
     },
   },
-  // do /portfolio if github page, do "" if kaijauregi.com
-  base: "",
 });
